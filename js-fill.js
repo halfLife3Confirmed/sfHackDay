8a7a59645f2138216ca57cb05de3f8361d92b2cb
Peter Bernardi <peter@kanudesigns.com>
2013-07-17 16:31:25 -0500

    Slowly inching towards functionality

--- js-fill.js
+++ js-fill.js
@@ -1,3 +1,9 @@
-var idea = new Object();
-	idea.title='Test idea';
-	
+var currentdate = new Date(); 
+var ideaArray = new Array();
+var idea = {
+	author : 'Peter Bernardi',
+	score : 0,
+	name : 'Test idea 1',
+	timestamp : currentdate.getHours() + ":" + currentdate.getMinutes(),
+	comments : ['comment 1', 'comment 2', 'comment 3']
+}
